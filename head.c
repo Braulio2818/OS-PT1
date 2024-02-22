50c1cf18b2dd3cea20b28aadd90d27f7b9ed5d29
Braulio  Bracamontes <126724180+Braulio2818@users.noreply.github.com>
2024-02-22 08:28:50 -0700

    added cases

--- head.c
+++ head.c
@@ -45,21 +45,64 @@ int main (int argc, char **argv) {
             /*copy first 10 lines of standard input*/
             char buffer[100];
             int i = 0;
+            int newline_count=0;//added count
             while (i < 10){
                 int n = read(0, buffer, 100);
                 if (n == 0){
                     break;
                 }
                 my_write(1, buffer, n);
+
+                for(int j=0;j<n;j++){
+                    if (buffer[j]== '\n'){
+                        newline_count++;
+                        if(newline_count==10){
+                            i=10;
+                            break;
+                        }
+                    }
                 i++;
             }
             break;
         case 2:
             /*copy first 10 lines of file*/
+            int fd = open(argv[1],O_RDONLY);
+            if (fd ==-1){
+                my_print("Error opening file\n");
+                return -1;
+            }
+            char buffer[100];
+            int line_count2= 0;//renamed the variable i to line_count2
+            while (line_count2 < 10){
+                int n = read(fd, buffer, 100);
+                if (n == 0){
+                    break;
+                }
+                my_write(1, buffer, n);
+                i++;
+            }
+            close(fd);
             
             break;
         case 3:
             /*copy first n lines of file*/
+            int fd = open(argv[2],O_RDONLY);
+            if(fd == -1){
+                my_print("Error opening file\n");
+                return -1;
+            }
+            int n= atoi(argv[1]);
+            char buffer[100];
+            int line_count3=0;//renamed variable i to line_count3
+            while(line_count3 < n){
+                int read_bytes = read(fd,buffer,100);
+                if(read_bytes==0){
+                    break;
+                }
+                my_write(1,buffer,read_bytes);
+                i++;
+            }
+            close(fd);
             break;
         default:
             my_print("Error: too many arguments\n");
